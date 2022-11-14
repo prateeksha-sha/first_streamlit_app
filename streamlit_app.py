440714f8a3ea3a2d947d9c7e2253ff9353b8912a
prateeksha <118170988+prateeksha-sha@users.noreply.github.com>
2022-11-14 15:35:02 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,5 @@ streamlit.header('Breakfast Menu')
 
 
 streamlit.text('Omega 3 and Blueberry Oatmeal')
-streamliy.text('Kale,Spinach and Rocket Smoothie')
+streamlit.text('Kale,Spinach and Rocket Smoothie')
 streamlit.text('Hard-Boiled free-range Egg')
