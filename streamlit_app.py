5cf2c8908021f203fc9ff6003c950f01426c5d3c
prateeksha <118170988+prateeksha-sha@users.noreply.github.com>
2022-11-17 13:04:06 +0530

    Update streamlit_app.py
    typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,7 @@ try:
        streamlit.dataframe(back_from_function)
 except URLError as e:
     streamlit.error()
-  streamlit.header("The fruit load list contains:")
+streamlit.header("The fruit load list contains:")
 #snowflake related functions
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
