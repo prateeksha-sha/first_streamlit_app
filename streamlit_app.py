6ebcf1c98b3c73f3fd24e95e2aaa56282727bb8d
prateeksha <118170988+prateeksha-sha@users.noreply.github.com>
2022-11-17 12:35:26 +0530

    Update streamlit_app.py
    typo indentation error

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,7 +38,6 @@ try:
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon" + fruit_choice)
         fruityvice_normalized = pandas.json_normalize(fruityvice_response.json()) 
         streamlit.dataframe(fruityvice_normalized)
-         
-         except URLError as e:
-              streamlit.error()
+except URLError as e:
+    streamlit.error()
         
