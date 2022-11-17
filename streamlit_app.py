1de739f722b79b26cfac2fd839abc6052c2beb05
prateeksha <118170988+prateeksha-sha@users.noreply.github.com>
2022-11-17 11:27:33 +0530

    Update streamlit_app.py
    for not running anything from troubleshoot part

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,6 +33,11 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon"
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json()) 
 # output it the screen as a table
 streamlit.dataframe(fruityvice_normalized)
+# don't run anything past here while we troubleshoot
+streamlit.stop()
+
+
+
 import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
