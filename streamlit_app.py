716e3729e0b4f10cf4ee7d58fc341fca4186bd0a
prateeksha <118170988+prateeksha-sha@users.noreply.github.com>
2022-11-17 13:08:56 +0530

    Update streamlit_app.py
    typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,7 +52,7 @@ def get_fruit_load_list():
  #add a button to load a fruit
 if streamlit.button('Get Fruit load list'):
    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-   my_data_rows = get_fruit_load_list
+   my_data_rows = get_fruit_load_list()
    streamlit.dataframe(my_data_rows)
 
 
