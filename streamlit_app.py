b4205034f52813e520f488b53b0f881ee0f28c69
prateeksha <118170988+prateeksha-sha@users.noreply.github.com>
2022-11-18 09:59:17 +0530

    Update streamlit_app.py
    typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,13 +42,14 @@ try:
        back_from_function = get_fruityvice_data(fruit_choice)
        streamlit.dataframe(back_from_function)
 except URLError as e:
-    streamlit.error()
-streamlit.header("The fruit load list contains:")
+    
+   # streamlit.error()
+#streamlit.header("The fruit load list contains:")
 #snowflake related functions
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
 #allow the end user to add fruit to the list     
-  def insert_row_snowflake(new_fruit):
+def insert_row_snowflake(new_fruit):
     with my_cnx.cursor() as my_cur: 
          my_cur.execute("insert into fruit_load_list values ('from streamlit')")     
          return "Thanks for adding " + new_fruit
